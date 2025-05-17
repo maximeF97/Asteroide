c2ab2a0dc36bd918c01a282517e317e6f95ed950
fuhrm.max <fuhrmann.maxime@hotmail.fr>
2025-05-17 10:22:17 +0200

    D: add circleshape.py

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ def main():
     print("Starting Asteroids!") 
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    Clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        
+        dt = Clock.tick(60) / 1000.0
+       
 
 if __name__ == "__main__":
     main()
