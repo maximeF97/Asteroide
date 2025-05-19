a161784f6537c703207fa2790fc343259ca75cb9
fuhrm.max <fuhrmann.maxime@hotmail.fr>
2025-05-19 09:53:26 +0200

    E: working on turn button

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player = Player.update(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
